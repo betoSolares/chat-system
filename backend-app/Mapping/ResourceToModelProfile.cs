38915af0e22e78c0aacc017ca8fd2b265d366eb1
Roberto Solares <betosolaresgar@gmail.com>
2019-11-24 18:06:10 -0600

    Add a new layer for mapping objects using AutoMapper

---
+++ backend-app/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using backend_app.Application.Resources;
+using backend_app.Domain.Models;
+
+namespace backend_app.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SignUpResource, Account>();
+        }
+    }
+}
