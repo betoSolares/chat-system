7020d147a521d77e59fdfec47762f3afadee077d
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-25 19:31:22 -0600

    New account's model

--- frontend-app/Models/SignUp.cs
+++ frontend-app/Models/SignUp.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace frontend_app.Models
+﻿namespace frontend_app.Models
 {
     public class SignUp
     {
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
