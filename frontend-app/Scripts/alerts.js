0140370ff975181fef8d9cdaf17555b18e62c7c3
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-25 20:50:10 -0600

    Js manager of the error messages to show

---
+++ frontend-app/Scripts/alerts.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    if ($('#state').val() == 'SUCCESS') {
+        $('#alertBox').show()
+        $('#alertBox').addClass('SUCCESS')
+        $('#message').text("La operación se realizo de manera exitosa");
+    } else if ($('#state').val() != "") {
+        $('#alertBox').show()
+        $('#alertBox').addClass('failed')
+        $('#message').text($('#error').val());
+    }
+})
+
+$('#closebtn').click(function () {
+    $('#alertBox').hide()
+})
