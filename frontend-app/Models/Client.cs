6a8fe29440e0b2a93afc9418d5149131f5189b22
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-25 19:30:37 -0600

    Uri model for the API

---
+++ frontend-app/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+​
+namespace frontend_app.Models
+{
+    public class Client
+    {
+        public Uri URI { get; private set; }
+​
+        public Client()
+        {
+            URI = new Uri("http://localhost:50676/api/");
+        }
+    }
+}
