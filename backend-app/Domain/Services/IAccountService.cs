193cfe6bf38d1d229f692bdd0a64f28193d2405c
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-28 16:18:13 -0600

    Accounts service interface

---
+++ backend-app/Domain/Services/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using backend_app.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace backend_app.Domain.Services
+{
+    public interface IAccountService
+    {
+        Task<Response<List<Account>>> GetSpecific(string username);
+        Task<Response<Account>> GetInformation(string username);
+    }
+}
