b658245a673d6aacfc833cafc62cec05baa0fa83
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-28 16:19:22 -0600

    Contacts service interface

---
+++ backend-app/Domain/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using backend_app.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace backend_app.Domain.Services
+{
+    public interface IContactService
+    {
+        Task<Response<Contact>> AcceptRequest(string username, string otheruser);
+        Task<Response<Contact>> NegateRequest(string username, string otheruser);
+        Task<Response<Contact>> DeleteRequest(string username, string otheruser);
+        Task<Response<Contact>> SentRequest(string username, string otheruser);
+        Task<Response<List<string>>> GetIncoming(string username);
+        Task<Response<List<string>>> GetContacts(string username);
+        Task<Response<List<string>>> GetSent(string username);
+    }
+}
