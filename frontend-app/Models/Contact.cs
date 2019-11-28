32751ce2e2f3d16775d8618b880c773a01df40d4
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-28 16:28:43 -0600

    New model of the contacts data

---
+++ frontend-app/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace frontend_app.Models
+{
+    public class Contact
+    {
+        public string Username { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public bool SentRequest { get; set; }
+    }
+}
