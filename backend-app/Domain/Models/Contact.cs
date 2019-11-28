2c63d110f04c83fb158d801d47c9194f646bab7b
Brenner Hernández <velasquezmochis@gmail.com>
2019-11-28 16:17:02 -0600

    Contacts data model

---
+++ backend-app/Domain/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace backend_app.Domain.Models
+{
+    public class Contact
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        [BsonElement("username")]
+        public string Username { get; set; }
+        
+        [BsonElement("sent")]
+        public List<string> Sent { get; set; }
+        
+        [BsonElement("incoming")]
+        public List<string> Incoming { get; set; }
+        
+        [BsonElement("contacts")]
+        public List<string> Contacts { get; set; }
+    }
+}
