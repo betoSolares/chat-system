6e3352c2a101eee2f9c57d72a05b1a770ed02c62
Roberto Solares <betosolaresgar@gmail.com>
2019-11-29 21:15:22 -0600

    Add the model for the new conversation

---
+++ backend-app/Domain/Models/Conversation.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+namespace backend_app.Domain.Models
+{
+    public class Conversation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("members")]
+        public List<string> Members { get; set; }
+        [BsonElement("messages")]
+        public List<Message> Messages { get; set; }
+    }
+}
