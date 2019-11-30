906b9dcdb5a6aed2b89e09cf0fe80e4b0dca9ff7
Roberto Solares <betosolaresgar@gmail.com>
2019-11-29 21:16:11 -0600

    Add the model for the message

---
+++ backend-app/Domain/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace backend_app.Domain.Models
+{
+    public class Message
+    {
+        [BsonElement("from")]
+        public string From { get; set; }
+        [BsonElement("content")]
+        public string Content { get; set; }
+        [BsonElement("isFile")]
+        public bool IsFile { get; set; }
+        [BsonElement("path")]
+        public string Path { get; set; }
+    }
+}
