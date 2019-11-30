2460327343c54d7dec85c35767cee7376c572ad6
Roberto Solares <betosolaresgar@gmail.com>
2019-11-29 21:23:31 -0600

    Add the new model for the conversations

---
+++ frontend-app/Models/Conversation.cs
@@ -0,0 +1,9 @@
+﻿namespace frontend_app.Models
+{
+    public class Conversation
+    {
+        public string Username { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
