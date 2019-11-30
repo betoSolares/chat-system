823a19137fc09cd039bad64be8f456fcaf521f8c
Roberto Solares <betosolaresgar@gmail.com>
2019-11-29 21:09:33 -0600

    Add the resource for the message

---
+++ backend-app/Application/Resources/MessageResource.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_app.Application.Resources
+{
+    public class MessageResource
+    {
+        public string From { get; set; }
+        public string Content { get; set; }
+        public bool IsFile { get; set; }
+        public string Path { get; set; }
+    }
+}
